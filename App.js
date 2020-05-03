ea171dcf2eff068336b64d47b713a568796a7d84
vincetita <vince.tita@gmail.com>
2020-05-03 22:17:14 +0200

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,39 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [people, setPeople] = useState([
+     {name:'Josh', age:6, id:1},
+    {name:'Ariel', age:4, id:2},
+    {name:'Tapita', age:9, id:3},
+    {name:'Ryan', age:12, id:4},
+    {name:'Leonel', age:16, id:5},
+    {name:'Vince', age:6, id:6},
+    {name:'Martin S', age:2, id:7},
+    {name:'Lloyd', age:15, id:8},
+  ]);
+
+  const pressHandler = (id) => {
+    console.log(id);
+    setPeople((prevPeople) => {
+      return prevPeople.filter(person => person.id  != id);
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <FlatList 
+        numColumns={2}
+        keyExtractor={(item) => item.id} 
+        data={people} 
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => pressHandler(item.id)}>
+            <Text style={styles.item}>{item.name}</Text>
+          </TouchableOpacity>
+        )}
+      />
+
     </View>
   );
 }
@@ -12,8 +41,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 40,
+    paddingHorizontal: 20,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  item: {
+    flex: 1,
+    marginHorizontal: 10,
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: 'pink',
+    fontSize: 24,
   },
 });
